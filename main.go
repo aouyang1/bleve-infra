80e9b9ec93c548bfd6e5c0083078f681b8e9fcc4
Austin Ouyang <aouyang@linkedin.com>
2018-10-18 21:30:51 +0530

    added example that uses ngrams and such for bleve

--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 	"github.com/blevesearch/bleve/analysis/analyzer/custom"
 	"github.com/blevesearch/bleve/analysis/token/lowercase"
 	"github.com/blevesearch/bleve/analysis/token/ngram"
-	"github.com/blevesearch/bleve/analysis/tokenizer/single"
+	"github.com/blevesearch/bleve/analysis/token/porter"
 	"github.com/blevesearch/bleve/analysis/tokenizer/whitespace"
+	"github.com/blevesearch/bleve/search/query"
 )
 
 // Message struct defines the document to be indexed
@@ -19,22 +20,14 @@ type Message struct {
 	Body string
 }
 
-// SingleTermAnalyzer is a custom analyzer
-func SingleTermAnalyzer() map[string]interface{} {
-	return map[string]interface{}{
-		"type":          custom.Name,
-		"char_filters":  []string{},
-		"tokenizer":     single.Name,
-		"token_filters": []string{},
-	}
-}
-
 func NgramAnalyzer() map[string]interface{} {
 	return map[string]interface{}{
 		"type":         custom.Name,
 		"char_filters": []string{},
 		"tokenizer":    whitespace.Name,
 		"token_filters": []string{
+			"stop_en",
+			porter.Name,
 			lowercase.Name,
 			"ngram_filter",
 		},
@@ -47,48 +40,91 @@ func NgramTokenFilter() map[string]interface{} {
 		"max":  float64(4),
 	}
 }
-func main() {
-	message := Message{
-		ID:   "example",
-		From: "marty.schoch@gmail.com",
-		Body: "bleve indexing is easy",
+
+func runQuery(index bleve.Index, query *query.MatchQuery) {
+	req := bleve.NewSearchRequest(query)
+	req.Fields = []string{"From", "Body"}
+
+	res, err := index.Search(req)
+	if err != nil {
+		log.Printf("%v\n", err)
 	}
 
-	var index bleve.Index
+	fmt.Printf("Query: %+v got %v\n", query, res)
+}
+
+func main() {
+	messages := []Message{
+		{
+			ID:   "0",
+			From: "marty.schoch@gmail.com",
+			Body: "bleve indexing is easy",
+		},
+		{
+			ID:   "1",
+			From: "aouyang1@gmail.com",
+			Body: "I'm Trying to learn bleve",
+		},
+		{
+			ID:   "2",
+			From: "souyang1@gmail.com",
+			Body: "why is Bleve hard?",
+		},
+		{
+			ID:   "3",
+			From: "blargh@gmail.com",
+			Body: "what is indexes asdf bevel",
+		},
+		{
+			ID:   "4",
+			From: "blargh@gmail.com",
+			Body: "what watery indexes asdf bevel",
+		},
+		{
+			ID:   "5",
+			From: "blargh@gmail.com",
+			Body: "water is something Like This In Wisconsin asdf bevel",
+		},
+	}
+
+	var idx bleve.Index
 	var err error
-	index, err = bleve.Open("example.bleve")
+	idx, err = bleve.Open("example.bleve")
 	if err != nil {
 		indexMapping := bleve.NewIndexMapping()
 		indexMapping.AddCustomTokenFilter("ngram_filter", NgramTokenFilter())
 		indexMapping.AddCustomAnalyzer("ngram_analyzer", NgramAnalyzer())
+		indexMapping.DefaultAnalyzer = "ngram_analyzer"
 
-		docMapping := bleve.NewDocumentMapping()
-		indexMapping.AddDocumentMapping("simple_doc", docMapping)
+		emailMapping := bleve.NewDocumentMapping()
+		emailFieldMapping := bleve.NewTextFieldMapping()
+		emailMapping.AddFieldMappingsAt("From", emailFieldMapping)
+		emailMapping.AddFieldMappingsAt("Body", emailFieldMapping)
 
-		rawFieldMapping := bleve.NewTextFieldMapping()
-		rawFieldMapping.Analyzer = "ngram_analyzer"
+		indexMapping.AddDocumentMapping("email", emailMapping)
 
-		docMapping.AddFieldMappingsAt("From", rawFieldMapping)
-		docMapping.AddFieldMappingsAt("Body", rawFieldMapping)
-
-		index, err = bleve.New("example.bleve", indexMapping)
+		idx, err = bleve.New("example.bleve", indexMapping)
 		if err != nil {
 			panic(err)
 		}
 
-		index.Index(message.ID, message)
+		for _, m := range messages {
+			idx.Index(m.ID, m)
+		}
 	}
 
-	query := bleve.NewMatchQuery("indexin")
-	query.SetField("Body")
+	var query *query.MatchQuery
+	query = bleve.NewMatchQuery("bleve")
+	runQuery(idx, query)
 
-	searchRequest := bleve.NewSearchRequest(query)
-	searchRequest.Fields = []string{"Body"}
+	query = bleve.NewMatchQuery("index")
+	runQuery(idx, query)
 
-	searchResult, err := index.Search(searchRequest)
-	if err != nil {
-		log.Printf("%v", err)
-	}
+	query = bleve.NewMatchQuery("watered")
+	runQuery(idx, query)
 
-	fmt.Printf("%+v", searchResult)
+	query = bleve.NewMatchQuery("ouya")
+	query.SetField("From")
+	runQuery(idx, query)
+
 }
